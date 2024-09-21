495512c98130708cfc5b9552007a904c69ea1ed5
star_ <star_berry2s@yahoo.co.jp>
2024-09-22 05:03:38 +0900

    診断結果

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Post.create([
+  { number: '5', content: 'スーパーパパ', image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiZbyFzKmTUzJg_jVoHOgyjOdkMK6773UNQ6FLX1BYa8EmSCK3RzgLypFz-vo__cYcbIjdpifIvRYoABh0NExeBVesi8Jvb60QlQOlm8hY4iEekxgTTsq54ehxZ2NxVTWP41uv9dsP5IjO-/s800/hero_man.png', description: 'いつも子供のことを考えて、積極的に子育てに関わるパパ。責任感があり、子供との時間を大切にし、何事にも全力で取り組む父親像です。' },
+  { number: '4', content: '思いやりパパ', image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi2qbSjaO0a1KB0QwoWORZblMWM59PBpz7hzrGhtucbkR4UoF27y9Ov1_ISdTgPj-jH-pLg0OJ1xbpsqMOSS2Lk1j_aC6imwljPl8OVpcNAsVbR2_7SpWJPlEzoH-Oy907iGBzkvf7a1Ixr/s1600/baby_jyunyu_honyubin_man.png', description: '子供に対して思いやりを持ち、状況に応じてサポートするパパ。忙しい時もあるけど、できる限り子供に寄り添おうとしています。' },
+  { number: '3', content: 'バランス型パパ', image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgY5ldxEgyZkoE8Y63WkTFbjmFeDu1dITSme6Ek5uUSr8xiG_3Cs-c6RfuNByiauwAEZQ607XEHxRDg8MyvL44rFQudskrpZpE7cJv0Hliphqb9b8EdBsdDTinkrDAfJEPky6oVkP_RoyKX/s800/baby_dakko_father.png', description: '時々子育てに関与し、他のこととバランスを取りながら子供に接しています。子供との関係も良好で、必要な時にはしっかりとサポートするタイプです。' },
+  { number: '2', content: '頑張りパパ', image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEglWaIhVJLtnBYiOV_PIfjIxu1JfQFRyTCwDO5XlMAQQMY6NpjhcCd6-B1U8g4cAMwk-CrE6gQ8aWbWRx3uplfoEIetkWkrmUuvlXNdjZN80FAf3Uw69aLH1Q2hUMbjBbzng6R_8GjHkHz4/s1600/shitsuke_shikaru_father_mu.png', description: '子育てに苦戦することも多いですが、努力はしています。まだ改善の余地はありますが、少しずつ親としての成長を目指しています。' },
+  { number: '1', content: '放任パパ', image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj1ibgWldk5frzbYyvMRZtQWV0S12CU287M3djxDTgQ0A_7I3285IYTOI5lrfYZPo4Tbnv0FXzZDe3J3i2VmNqivX4UO4vaAEYinlqQ5bSPPNSmAczMWG1Gug_Sk1CYGiJ5PfmhUa2pJFtr/s800/smartphone_man_walk.png', description: '子育てにあまり関心がなく、パートナーに任せきりのタイプ。子供との関わりを増やすための努力が必要です。' }
+])
